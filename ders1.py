0ebc0e2a2b4d22d1ce15c1cc138a3e118c0be9b2
Nasibov1995 <nasib_az@mail.ru>
2024-11-21 12:36:50 +0400

    teze

---
+++ ders1.py
@@ -0,0 +1,99 @@
+
+#  str()   string   text = "Python"
+# int()  integer  number1 = 5
+# float()  float   float_number = 1.5
+# bool()  bool  bool_number = False
+#  type() type 
+#  = menimsetmek
+#  == beraberdir
+#  != beraber deyil
+#  len()   saymaq ucun
+#  % qaligi tapmaq ucun
+
+text = 'Python version'
+
+number1 = 5
+
+float_number = 1.5
+
+bool_number = True
+
+
+print("55")
+
+print(type(text))
+print(type(number1))
+print(type(text))
+
+print(type(bool_number))
+
+a = 10
+b = 10
+
+print(a!=b)
+
+name = "Ali"
+
+print(name[1])
+
+
+print(name.upper())
+
+print(name.lower())
+
+print(name.count("a",1))
+
+
+print(name.replace("li","slan"))
+
+
+name.replace("you must enable javasript","")
+
+print(name.replace("  ","test"))
+
+name = "Nasib"
+
+
+print(len(name))
+
+x = "Welcome"
+number = "5"
+
+
+print(x+number)
+print(type(number))
+
+
+name = "Ali"
+city = "Baku"
+
+
+print(f"{name} live in {city}")
+print("{} live in {}".format(name,city))
+
+print(10 // 3)
+
+number1 = 5
+number2 = 2
+
+print(number1 % number2)
+
+print("Python Programming \tLanguage ")
+
+
+name = "Python Programming Language"
+
+print(name.strip())
+
+print(name.split(""))
+
+
+name = input("Adinizi daxil edin : ")
+
+print(f"Welcome {name}")
+
+
+number = str(input("Number: "))
+
+print(number)
+
